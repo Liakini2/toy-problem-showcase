14110a7cdbb83c1f04e9a34c4b42865c8b9bd0b3
Lia Couch <76275077+Liakini2@users.noreply.github.com>
2021-01-20 17:12:49 -0700

    filter objects

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -3,9 +3,62 @@
 import React, {Component} from 'react';
 
 class FilterObjects extends Component{
+    constructor(){
+        super()
+        this.state = {
+            unFilteredArray : [
+                {
+                    toy: 'Barbie',
+                    color: 'Pink',
+                    career: "doctor",
+                    owned: 5,
+                },
+                {
+                    toy: 'Hot Wheels',
+                    color: 'Red',
+                    owned: 4,
+                    age: 3,
+                }, 
+                {
+                    toy: 'Legos',
+                    number: 350,
+                }
+            ],
+            userInput: '',
+            filteredArray: [],
+        }
+    }
+
+    UpdateUserInput = (value) =>{
+        this.setState({
+            userInput: value
+        })
+    }
+
+    UpdateFilteredArray = (prop) =>{
+        let arr = this.state.unFilteredArray
+        let filteredArray = []
+
+        for(let i=0; i < arr.length; i++){
+            if(arr[i].hasOwnProperty(prop)){
+                filteredArray.push(arr[i])
+            } 
+        }
+        this.setState({filteredArray: filteredArray})
+    }
+
     render(){
         return(
-            <p>Filter Objects</p>
+            <div className='puzzleBox filterObjectPB'>
+                <h4>"Filter Object"</h4>
+                <span className='puzzleText'>Unfiltered Array: {JSON.stringify(this.state.unFilteredArray, null, 10)}</span>
+                <input className='inputLine'
+                onChange={ (e) => this.UpdateUserInput(e.target.value)}/>
+                <button className='confirmationButton'
+                onClick={()=>this.UpdateFilteredArray(this.state.userInput)}
+                >Filter</button>
+                <span className='resultsBox filterObjectRB'>FilteredArray: {JSON.stringify(this.state.filteredArray, null, 10)}</span>
+            </div>
         )
     }
 }
